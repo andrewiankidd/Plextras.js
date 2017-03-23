d7b4ca51541c0d6633e47b9e259ea96f98f2e31c
Andrew Kidd <contact@andrewiankidd.co.uk>
2017-03-23 00:01:16 +0000

    Update Plextras.js
    add minimizing sidebar + @uzegonemad artwork fix

--- Plextras.js
+++ Plextras.js
@@ -3,6 +3,8 @@
 ////////////////////////////////////////////////////
 var delayInt = 2500;
 var cssBGColor = '#3f4245';
+var autohideSidebar = true;
+var showArtWorkBackground = true;
 var customHeader = "Custom Links";
 var customLinks = {'Home':'javascript:switchPort(80)', 'Requests':'javascript:switchPort(3000)', 'Uptime':'https://stats.uptimerobot.com/q7BGEHzZz'};
 
@@ -23,16 +25,88 @@ document.getElementById('content').style.backgroundColor = cssBGColor;
 //NOTE: this doesnt play well with user chooser (plex home)
 //NOTE: would be nice to have a better trigger
 window.onload = function () {	
+	//start
+	console.log('[Plextras.js] Starting Plextras...');
+	loadCustomStyles();
+
+	//wait a bit to load custom section
 	setTimeout(
 		function() 
-		{
+		{					
 			loadCustomSection();
+			loadSidebarSettings();
 		},
 	delayInt);	
 }
 
+function loadCustomStyles(){
+	
+	
+	if (showArtWorkBackground==true)
+	{
+		//all credit to https://github.com/uzegonemad/plexbgartwork
+		console.log('[Plextras.js] Adding artwork mod');
+		$.get( "https://raw.githubusercontent.com/andrewiankidd/plexbgartwork/master/plexbgartwork.css", function( data ) {
+		  $('head').append('<style type="text/css">'+ data +'</style>');
+		});
+	}
+}
 
-function loadCustomSection(){		
+function loadSidebarSettings(){
+	
+	//hide sidebar
+	if (autohideSidebar==true){
+		
+		//kill container margin and add transition speed
+		$(".page-container").css("margin-left", "0px");	
+		$('.sidebar-container').css("transition", "0.25s");
+		
+		//add hover actions
+		$('.sidebar-container').hover(function(){
+			expandSidebar();
+		}, function(){
+			contractSidebar();
+		});	
+		
+		//contract sidebar
+		contractSidebar();
+	}	
+	
+	function expandSidebar(){
+		console.log('[Plextras.js] Expanding sidebar');
+		$('.sidebar-container').css("width", "240px");
+		//this has a short delay so it waits for transition animation to finish before restoring text
+		setTimeout(
+			function() 
+			{					
+				$('[class^="SidebarLink-title"]').removeAttr( 'style' );
+				$('[class^="SidebarServerLibraries-librariesTitle"]').removeAttr( 'style' );
+				$('[class^="SidebarList-sidebarListHeader"]').removeAttr( 'style' );
+				$('[class^="SidebarLibrariesActions-actions"]').removeAttr( 'style' );
+				$('[class^="SidebarLink-children"]').removeAttr( 'style' );
+				$('[class^="ServerMenuButton-serverMenuTitle"]').removeAttr( 'style' );
+				$('[class^="SidebarLibraryItem-action"]').removeAttr( 'style' );
+			},
+		300);			
+	}
+	
+	function contractSidebar(){
+		console.log('[Plextras.js] Contracting sidebar');
+		$('[class^="SidebarLink-title"]').css("font-size", "0px");
+		$('[class^="SidebarServerLibraries-librariesTitle"]').css("font-size", "0px");
+		$('[class^="SidebarList-sidebarListHeader"]').css("font-size", "0px");
+		$('[class^="SidebarLibrariesActions-actions"]').css("display", "none");
+		$('[class^="SidebarLink-children"]').css("display", "none");
+		$('[class^="ServerMenuButton-serverMenuTitle"]').css("display", "none");
+		$('[class^="SidebarLibraryItem-action"]').css("width", "0px");
+		$('.sidebar-container').css("width", "65px");
+	}
+}
+
+
+function loadCustomSection(){	
+	console.log('[Plextras.js] Adding custom links section');
+	
 	//locate navigation sidebar
 	var navdiv = $('div[role="navigation"]').parent();
 	
@@ -45,18 +119,17 @@ function loadCustomSection(){
 	
 	//copy Settings link as template
 	var linktemplate = newsec.find("div:eq(1)");
-	console.log('linktemplate: ' + linktemplate.html());	
 	
 	//remove all default links
-	newsec.find('a[aria-role="link"]').parent().remove();	
+	newsec.find('[class^="SidebarListItem-sidebarListItem"]').remove();	
 	
 	//start appending custom links
 	for (var key in customLinks) {
 		newsec = newsec.clone();
 		var newlink = linktemplate;	  
-		newlink.find('a[aria-role="link"]').attr("href", customLinks[key]);
-		newlink.find('a[aria-role="link"]').html(key);
-		newlink.find('a[aria-role="link"]').attr('target', '_blank');	
+		newlink.find('a').attr("href", customLinks[key]);
+		newlink.find('[class^="SidebarLink-title"]').html(key);
+		newlink.find('a').attr('target', '_blank');	
 		newsec.append(newlink);
 	}
 
@@ -68,6 +141,6 @@ function loadCustomSection(){
 function switchPort(port)
 {
 	var newurl = "//" + location.hostname + ":" + port;
-	console.log('newurl: ' + newurl)
+	console.log('newurl: ' + newurl);
 	window.location.href = newurl;
 }
