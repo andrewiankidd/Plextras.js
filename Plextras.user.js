03d9a50b597c4098fab19e1e396b2259b7a3eefb
Andrew Kidd <contact@andrewiankidd.co.uk>
2017-03-23 00:18:40 +0000

    Add userscript version

---
+++ Plextras.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Plextras.js Userscript version
+// @version      1
+// @author       Andrewiankidd.co.uk
+// @match        *://app.plex.tv/*
+// @include     /^http(s)?:\/\/(.*):32400\//
+// @downloadURL https://github.com/andrewiankidd/Plextras.js/blob/master/Plextras.user.js
+// @updateURL https://github.com/andrewiankidd/Plextras.js/blob/master/Plextras.user.js
+// ==/UserScript==
+
+(function () {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.rawgit.com/andrewiankidd/Plextras.js/master/Plextras.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(script);
+})();
