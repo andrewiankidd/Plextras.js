ee3ee7bdb329ea3f2c76c28fbc6eac416648d968
Andrew Kidd <contact@andrewiankidd.co.uk>
2017-03-28 09:21:10 +0100

    User.js no longer version hardcoded
    ditch rawgit for custom fix

--- Plextras.user.js
+++ Plextras.user.js
@@ -10,7 +10,7 @@
 
 (function () {
     var script = document.createElement('script');
-    script.src = 'https://cdn.rawgit.com/andrewiankidd/Plextras.js/c6cc8ebd6932b8d97a9e9fb944ef056dc894ed6d/Plextras.js';
+    script.src = 'http://andrewiankidd.co.uk/ext/mimefix/?gitfile=Plextras.js';
     script.type = 'text/javascript';
     document.getElementsByTagName('head')[0].appendChild(script);
 })();
